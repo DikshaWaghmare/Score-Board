77b59438efb1b6331864983750469bbe203b167d
DikshaWaghmare <dikshaw44@gmail.com>
2022-01-30 14:54:41 +0530

    add file

---
+++ score-board.js
@@ -0,0 +1,40 @@
+function incA(){
+    var scoreA=parseInt(document.getElementById("ScoreA").innerHTML);
+    scoreA++;
+    document.getElementById("ScoreA").innerHTML=scoreA;       
+}
+function decA(){
+    var scoreA=parseInt(document.getElementById("ScoreA").innerHTML);
+    scoreA--;
+    document.getElementById("ScoreA").innerHTML=scoreA;
+}
+function incB(){
+    var scoreB=parseInt(document.getElementById("ScoreB").innerHTML);
+    scoreB++;
+    document.getElementById("ScoreB").innerHTML=scoreB;
+}
+function decB(){
+    var scoreB=parseInt(document.getElementById("ScoreB").innerHTML);
+    scoreB--;
+    document.getElementById("ScoreB").innerHTML=scoreB;
+}
+function bonusA(){
+    var scoreA=parseInt(document.getElementById("ScoreA").innerHTML);
+    scoreA++;
+    document.getElementById("ScoreA").innerHTML=scoreA;
+}
+function AlloutA(){
+    var scoreA=parseInt(document.getElementById("ScoreA").innerHTML);
+    scoreA++;
+    document.getElementById("ScoreA").innerHTML=scoreA+1;
+}
+function bonusB(){
+    var scoreB=parseInt(document.getElementById("ScoreB").innerHTML);
+    scoreB++;
+    document.getElementById("ScoreB").innerHTML=scoreB;
+}
+function AlloutB(){
+    var scoreB=parseInt(document.getElementById("ScoreB").innerHTML);
+    scoreB++;
+    document.getElementById("ScoreB").innerHTML=scoreB+1;
+}
